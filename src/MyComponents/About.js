2409bdc511157069c8f111d6985b80ca81ce382c
AhanaKundu01 <ahanakundu01@gmail.com>
2021-07-06 20:51:02 +0530

    deploy my react app to github pages

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component.
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero voluptatibus aut facere nostrum sapiente nemo nulla nam adipisci in voluptate modi magnam, soluta odit repellendus iusto. Rerum dolores illo quidem tempora aliquam nisi laborum.</p>
+        </div>
+    )
+}
